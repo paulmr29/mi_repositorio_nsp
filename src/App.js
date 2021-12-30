f5e1254557e8deacc0b69d96139aba5f895c7976
PaulMR <paulkiss29@live.com.ar>
2021-12-30 16:24:46 -0300

    Primeros archivos repositorio

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" > NSP < header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p >
+        Mi primera tienda <
+        /p> <
+        a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" > Coderhouse < /a>    <
+        /header>    <
+        /div>
+    );
+}
+
+export default App;
